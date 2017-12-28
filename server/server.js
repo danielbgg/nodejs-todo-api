e8b348b09bae39fbd553f2346bc609c6250d49b7
kerdlix <danielbgg@gmail.com>
2017-12-28 15:08:27 -0200

    Lecture 78

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +29,25 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    console.log('Id is not valid', id);
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      console.log('Todo not found', id);
+      return res.status(404).send();
+    }
+    console.log('Todo By Id', todo);
+    res.status(200).send({todo});
+  }).catch((e) => {
+    console.log('Error fetching todo by id', e);
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
